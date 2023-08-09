663d2785a37b0c6600b6409bb1faa2666d283b23
Billy Burch <billyburch40@gmail.com>
2023-08-09 09:54:37 -0600

    Add lots of practice

---
+++ writing_methods_practice/writing_methods.rb
@@ -0,0 +1,94 @@
+# def greet_your_bro(name)
+#     return "What up broheemus, #{name}?!"
+# end
+
+# p greet_your_bro("Matt")
+
+# def greet_an_animal(animal)
+#     return "Look at that cute #{animal}"
+# end
+
+# p greet_an_animal("dinosaur")
+
+# def multiply(int1, int2)
+#     return product = (int1 * int2)
+# end
+
+# p multiply(2, 4)
+
+# def multiply2(int3, int4)
+#     int3 * int4
+# end
+
+# product1 = multiply2(4, 5)
+# product2 = multiply2(5, 6)
+
+# puts product1
+# puts product2
+
+# p multiply2(4, 6)
+
+# def say_hello(name)
+#     "Well hello there #{name}!"
+# end
+
+# p say_hello("Billy")
+
+# def get_age(year)
+#     2023 - year
+# end
+
+# mom = get_age(1941)
+# dad = get_age(1944)
+# sis = get_age(1978)
+
+# puts mom
+# puts dad
+# puts sis
+
+# def get_age(year)
+#     "You have lived #{2023 - year} years of life!"
+# end
+
+# mom = get_age(1941)
+# dad = get_age(1944)
+# sis = get_age(1978)
+
+# puts mom
+# puts dad
+# puts sis
+
+# arrays = ["billy", "bob", "joe", "jake", "aimee"]
+# p arrays[2]
+# p arrays[4]
+# p arrays[-2]
+# p arrays [0]
+
+# travel = ["reno", "las vegas", "bahamas", "puerto rico", "oaxaca"]
+
+
+# travel.each do |city|
+#     # print city.capitalize
+#     if city.include?(" ")
+#         words = city.split
+#         words.each do |word|
+#             word.capitalize
+#         end
+#     else
+#         puts city.capitalize
+#     end
+# end
+
+gets_sunshine = true
+ounces_of_water = 3
+
+if gets_sunshine = true && ounces_of_water >= 10
+    p "This plant will thrive!"
+elsif gets_sunshine = true || ounces_of_water >= 10
+    p "You're halfway there."
+else 
+    p "This plant will die."
+end
+
+
+
